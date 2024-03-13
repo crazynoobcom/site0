30c2d77810a67469b6ae9b9fc514b25779945192
xxLnolan <136196109+xxLnolan@users.noreply.github.com>
2024-03-13 20:00:26 +0100

    a

---
+++ up lfs zip.bat
@@ -0,0 +1,9 @@
+git lfs track "*.zip"
+
+git add .gitattribtues
+
+cmd /min /C git add %1
+
+git commit -m "a"
+
+git push origin main
--- up lfs.bat
+++
@@ -1,9 +0,0 @@
-git lfs track "*.zip"
-
-git add .gitattribtues
-
-cmd /min /C git add %1
-
-git commit -m "a"
-
-git push origin main
